67e19182dcdd82f62a4d3f8be05cf9c3289118ca
sinanmohd <sinan@sinanmohd.com>
2025-04-29 00:20:34 +0530

    ui/components/upload/files: init upload list

--- components/upload/files.vue
+++ components/upload/files.vue
@@ -7,7 +7,7 @@
       </CardDescription>
     </CardHeader>
     <CardContent class="space-y-2">
-      <div @click="clickUploadInput" @dragover="dragover" @dragleave="dragleave" @drop="drop" class="border-2 border-dashed p-16 rounded-lg">
+      <div @click="clickUploadInput" @dragover="dragover" @dragleave="dragleave" @drop="drop" class="border-2 border-dashed p-16 rounded-lg sm:hover:bg-accent">
         <div class="w-min mx-auto">
           <Icon v-if="isDragging" name="mdi:add" class="text-7xl text-muted-foreground"/>
           <Icon v-else name="mdi:cloud-upload-outline" class="text-7xl text-muted-foreground"/>
@@ -16,6 +16,23 @@
         <p v-else class="text-sm text-muted-foreground text-center">Drag & drop files here, or click to select files</p>
       </div>
       <input ref="uploadInput" type="file" multiple="true" class="invisible" @change="addInput" >
+
+      <div v-for="file in files"q class="border rounded-lg p-2 flex justify-between gap-2">
+        <div class="flex gap-2 truncate">
+          <Icon name="uil:file"  class="text-4xl my-auto"/>
+          <div>
+            <div class="font-bold my-auto truncate">
+              {{ file.name }}
+            </div>
+            <div class="text-muted-foreground text-sm">
+              {{ file.size }}
+            </div>
+          </div>
+        </div>
+        <Button variant="ghost" class="my-auto">
+          <Icon name="mdi:close" />
+        </Button>
+      </div>
     </CardContent>
     <CardFooter class="flex justify-end">
       <Button @click="upload">Upload</Button>
@@ -28,17 +45,21 @@ export default {
   data() {
     return {
       isDragging: false,
-      files: new Set(),
+      files: new Array(),
     };
   },
   methods: {
     upload() {
-       console.log(this.files);
-      this.files.clear();
+        console.log(this.files)
+      this.files = new Array();
     },
     drop(event: DragEvent) {
       event.preventDefault();
-      this.files.add(event.dataTransfer?.files);
+
+      if (!event.dataTransfer) {
+        return
+      }
+      this.files = [...this.files, ...event.dataTransfer.files];
     },
     dragover(event: DragEvent) {
       event.preventDefault();
@@ -49,7 +70,11 @@ export default {
     },
     addInput(event: Event) {
       const el = event.target as HTMLInputElement;
-      this.files.add(el.files);
+
+      if (!el.files) {
+        return
+      }
+      this.files = [...this.files, ...el.files];
     },
     clickUploadInput() {
       const el = this.$refs.uploadInput as HTMLInputElement;
