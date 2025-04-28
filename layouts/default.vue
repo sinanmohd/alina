af1323df547805871d3ab61cfb3df35033f153c8
sinanmohd <sinan@sinanmohd.com>
2025-04-28 19:35:39 +0530

    ui: init

---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <main class="flex flex-col min-h-screen">
+    <header>
+      <nav class="sm:w-7xl w-full px-3 mx-auto py-3 flex justify-between">
+        <section class="flex gap-1 text-3xl">
+          <Icon name="my-icon:logo" style="color: red"/>
+          <p class="font-bold text-red-700">alina</p>
+        </section>
+
+        <section class="flex">
+          <Button variant="ghost">
+            <a href="https://github.com/sinanmohd/alina" target="_blank">
+              <Icon name="uil:github" class="text-2xl"/>
+            </a>
+          </Button>
+        </section>
+      </nav>
+    </header>
+    <Separator />
+
+    <div class="flex-grow">
+      <slot />
+    </div>
+
+    <Separator />
+    <footer class="sm:w-7xl w-full px-3 mx-auto py-2 flex justify-between">
+      <p class="text-sm text-(--ui-text-muted)">
+        Copyright © {{ new Date().getFullYear() }} Sinan -
+        <a href="https://www.gnu.org/licenses/agpl-3.0.en.html" target="_blank" class="hover:underline"> AGPL3 License </a>
+      </p>
+
+      <section class="flex">
+        <Button variant="ghost">
+          <a href="https://www.sinanmohd.com/" target="_blank">
+            <Icon name="mdi:web" class="text-2xl"/>
+          </a>
+        </Button>
+        <Button variant="ghost">
+          <a href="https://www.linkedin.com/in/sinanmohd/" target="_blank">
+            <Icon name="uil:linkedin" class="text-2xl"/>
+          </a>
+        </Button>
+        <Button variant="ghost">
+          <a href="https://github.com/sinanmohd" target="_blank">
+            <Icon name="uil:github" class="text-2xl"/>
+          </a>
+        </Button>
+      </section>
+    </footer>
+  </main>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  modules: ['shadcn-nuxt', '@nuxt/icon'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    componentDir: './components/ui'
+  },
+
+  icon: {
+    customCollections: [
+      {
+        prefix: 'my-icon',
+        dir: './assets/my-icons'
+      },
+    ],
+  },
 })
