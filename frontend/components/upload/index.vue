9c8f09c3a7bc0bd04db51ba6c2eae63586a92900
sinanmohd <sinan@sinanmohd.com>
2025-05-08 11:00:52 +0530

    ui/components/upload: drop terminal

--- frontend/components/upload/index.vue
+++ frontend/components/upload/index.vue
@@ -1,7 +1,7 @@
 <template>
   <Tabs default-value="files" class="w-full sm:w-3xl mx-auto p-4">
 
-    <TabsList class="grid w-full grid-cols-3">
+    <TabsList class="grid w-full grid-cols-2">
       <TabsTrigger value="files">
         <Icon name="mdi:files"/>
         Files
@@ -9,10 +9,6 @@
       <TabsTrigger value="text">
         <Icon name="mdi:text"/>
         Text
-      </TabsTrigger>
-      <TabsTrigger value="terminal">
-        <Icon name="mdi:terminal"/>
-        Terminal
       </TabsTrigger>
     </TabsList>
 
@@ -22,10 +18,6 @@
 
     <TabsContent value="text">
       <UploadText />
-    </TabsContent>
-
-    <TabsContent value="terminal">
-      <UploadTerminal />
     </TabsContent>
   </Tabs>
 </template>
