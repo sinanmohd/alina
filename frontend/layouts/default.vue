2fbb7bf100c89ea160b735674853f38364a8f2b2
sinanmohd <sinan@sinanmohd.com>
2025-05-11 23:20:18 +0530

    ui/layout: fix footer width

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -25,29 +25,31 @@
 
     <Separator />
 
-    <footer class="sm:w-7xl w-full px-3 mx-auto py-2 flex justify-between">
-      <p class="text-sm text-(--ui-text-muted) my-auto">
-        Copyright © {{ new Date().getFullYear() }} Sinan -
-        <a href="https://www.gnu.org/licenses/agpl-3.0.en.html" target="_blank" class="hover:underline"> AGPL3 License </a>
-      </p>
+    <footer>
+      <div class="max-w-7xl px-3 mx-auto py-2 flex justify-between">
+        <p class="text-sm text-(--ui-text-muted) my-auto">
+          Copyright © {{ new Date().getFullYear() }} Sinan -
+          <a href="https://www.gnu.org/licenses/agpl-3.0.en.html" target="_blank" class="hover:underline"> AGPL3 License </a>
+        </p>
 
-      <section class="flex">
-        <Button variant="ghost">
-          <a href="https://www.sinanmohd.com/" target="_blank">
-            <Icon name="lucide:globe" class="text-2xl"/>
-          </a>
-        </Button>
-        <Button variant="ghost">
-          <a href="https://www.linkedin.com/in/sinanmohd/" target="_blank">
-            <Icon name="uil:linkedin" class="text-2xl"/>
-          </a>
-        </Button>
-        <Button variant="ghost">
-          <a href="https://github.com/sinanmohd" target="_blank">
-            <Icon name="uil:github" class="text-2xl"/>
-          </a>
-        </Button>
-      </section>
+        <section class="flex">
+          <Button variant="ghost">
+            <a href="https://www.sinanmohd.com/" target="_blank">
+              <Icon name="mdi:web" class="text-2xl"/>
+            </a>
+          </Button>
+          <Button variant="ghost">
+            <a href="https://www.linkedin.com/in/sinanmohd/" target="_blank">
+              <Icon name="uil:linkedin" class="text-2xl"/>
+            </a>
+          </Button>
+          <Button variant="ghost">
+            <a href="https://github.com/sinanmohd" target="_blank">
+              <Icon name="uil:github" class="text-2xl"/>
+            </a>
+          </Button>
+        </section>
+      </div>
     </footer>
   </main>
 </template>
